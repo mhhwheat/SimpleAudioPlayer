36af4c1e7f2d2d2a418e040abb9fec0fc400305e
mhhwheat <1057251411@qq.com>
2016-04-11 16:55:16 +0800

    add SimpleAudioPlayerActivity

---
+++ app/src/main/java/com/wheat/mobile/simpleaudioplayer/SimpleAudioPlayer.java
@@ -0,0 +1,15 @@
+package com.wheat.mobile.simpleaudioplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/4/11.
+ */
+public class SimpleAudioPlayer extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
